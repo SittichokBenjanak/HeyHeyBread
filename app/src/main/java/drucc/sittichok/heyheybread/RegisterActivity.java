74e0e5f8621aee3f5137b1ef55b6c49e2e695d2b
mos <moszajang@outlook.com>
2016-01-02 13:32:25 +0700

    Check Space แล้ว

--- app/src/main/java/drucc/sittichok/heyheybread/RegisterActivity.java
+++ app/src/main/java/drucc/sittichok/heyheybread/RegisterActivity.java
@@ -2,6 +2,7 @@ package drucc.sittichok.heyheybread;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
@@ -23,7 +24,41 @@ public class RegisterActivity extends AppCompatActivity {
 
     }   // onCreate
 
-    private void bindWidget() {
+    public void clickSave(View view) {
 
+        //Check Space
+        userString = UserEditText.getText().toString().trim();
+        passwordString = PasswordEditText.getText().toString().trim();
+        nameString = NameEditText.getText().toString().trim();
+        surnameString = SurnameEditText.getText().toString().trim();
+        addressString = AddressEditText.getText().toString().trim();
+        phoneString = PhonEditText.getText().toString().trim();
+
+        if (userString.equals("") ||
+                passwordString.equals("") ||
+                nameString.equals("") ||
+                surnameString.equals("") ||
+                addressString.equals("") ||
+                phoneString.equals("")) {
+
+            //Have Space
+            MyAlertDialog objMyAlertDialog = new MyAlertDialog();
+            objMyAlertDialog.errorDialog(RegisterActivity.this, "Have Space", "กรุณากรอกทุกช่อง");
+
+        } else {
+
+            //No Space
+
+        } // if
+
+    }   //clickSave
+
+    private void bindWidget() {
+        UserEditText = (EditText) findViewById(R.id.edtUser);
+        PasswordEditText= (EditText) findViewById(R.id.edtPass);
+        NameEditText = (EditText) findViewById(R.id.edtName);
+        SurnameEditText = (EditText) findViewById(R.id.edtSurname);
+        AddressEditText = (EditText) findViewById(R.id.edtAddress);
+        PhonEditText = (EditText) findViewById(R.id.edtPhone);
     }
 }   // Main class
