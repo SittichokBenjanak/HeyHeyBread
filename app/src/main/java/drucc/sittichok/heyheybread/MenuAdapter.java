a11961e48a6e6468dd1551d0cd95a4ed374b3929
mos <moszajang@outlook.com>
2016-01-03 13:02:07 +0700

    Desing Menu แล้ว

---
+++ app/src/main/java/drucc/sittichok/heyheybread/MenuAdapter.java
@@ -0,0 +1,9 @@
+package drucc.sittichok.heyheybread;
+
+/**
+ * Created by mosza_000 on 3/1/2559.
+ */
+public class MenuAdapter {
+
+
+}   //Main Class
---
+++ app/src/main/java/drucc/sittichok/heyheybread/showMenuActivity.java
@@ -0,0 +1,13 @@
+package drucc.sittichok.heyheybread;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class showMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_menu);
+    }
+}
