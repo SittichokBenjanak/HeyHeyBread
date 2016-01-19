767aae54ddc2514db76422a2624183a1ff2f9bb4
mos <moszajang@outlook.com>
2016-01-19 16:52:47 +0700

    บายวิเก็ตแล้ว

--- app/src/main/java/drucc/sittichok/heyheybread/ConfirmOrderActivity.java
+++ app/src/main/java/drucc/sittichok/heyheybread/ConfirmOrderActivity.java
@@ -2,13 +2,36 @@ package drucc.sittichok.heyheybread;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class ConfirmOrderActivity extends AppCompatActivity {
 
+    // Explicit
+    private TextView dateTextView, nameTextView,addressTextView,
+            phoneTextView,totalTextView;
+    private String dateString,nameString,addressString,
+            phoneString,totalString;
+    private ListView orderListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm_order);
+
+        // Bind Widget
+        bindWidget();
+
     }   // Main Method
+
+    private void bindWidget() {
+
+        dateTextView = (TextView) findViewById(R.id.textView19);
+        nameTextView = (TextView) findViewById(R.id.textView20);
+        addressTextView = (TextView) findViewById(R.id.textView21);
+        phoneTextView = (TextView) findViewById(R.id.textView22);
+        totalTextView = (TextView) findViewById(R.id.textView23);
+        orderListView = (ListView) findViewById(R.id.listView2);
+
+    }   //bindWidget
 
 }   // Main class
